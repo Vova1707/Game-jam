1b29c84ef6a7aeb0c493dabab8e8dce8285000f5
SpaceLink <128892453+Andrew-24coop@users.noreply.github.com>
2024-12-23 20:15:52 +0300

    added description to refer.py

--- refer.py
+++ refer.py
@@ -1,17 +1,45 @@
+from cProfile import label
+
 import pygame
+
+main_text = ('  Добро пожаловать в репозиторий "Геймификация рекламы для',
+             '                      компьютерного клуба" хакатона Game-Jam',
+             '                                      Описание кейса',
+             '                              Описание версий проекта',
+             '                          Как ориентироваться в проекте',
+             '          Ветки:',
+             '- main - основная ветка - туда мы кидаем релизы и версии проекта',
+             '- develop(pref) - разработка(подготовка) - это ветка разработки',
+             '                          версий проекта перед началом хакатона',
+             '- develop - разработка - это ветка основной разработки',
+             '                          (разработка проекта во время хакатона)',
+             '- feature/... - папка основных фичей - туда мы будут ветки основных',
+             'фичей, который разрабатывались во время хакатона',
+             '- feature(pref)/... - папка фичей(подготовка) -',
+             'папка, в которой лежат ветки фичей, которые мы',
+             'разрабатывали, когда готовились к хакатону',
+             '                                      Описание коммитов:',
+             '         Коммиты main:',
+             'vX.X | краткое описание',
+             '- X - версию на данный момент',
+             '- краткое описание - общие добавления в новую версию',
+             'Коммиты в остальных ветках название коммиты |',
+             '      краткое описание, что добавилось',
+             "- Например 'start commit | перенёс всю свою игру'")
 
 class Refer:
     def __init__(self, parent, base_style):
+        self.labels = []
         self.base_style = base_style
         self.parent = parent
         self.buttons = []
         self.init_label_title()
+        self.init_main_text()
         self.init_button_menu()
 
     def init_label_title(self):
-        self.labels = []
         label_title = {
-            "coords": (100, 20),
+            "coords": (100, 10),
             "text":"Справка",
             "font": pygame.font.Font(self.base_style["font_path"], 50)
         }
@@ -21,6 +49,21 @@ class Refer:
         label_title["label"], label_title["coords"] = self.parent.align(label_title["label"], label_title["coords"],
                                                             inacurr=-20, type_blit=False, type_align="horizontal")
         self.labels.append(label_title)
+
+    def init_main_text(self):
+        txt = [text for text in main_text]
+        for i, mini_t in enumerate(txt):
+            label_title = {
+                "coords": (50, 80 + i * 30),
+                "text": mini_t,
+                "font": pygame.font.Font(self.base_style["font_path"], 15)
+            }
+            label_title["label"] = self.parent.label_text(coords=label_title["coords"],
+                                                          text=label_title["text"],
+                                                          font=label_title["font"])
+            #label_title["label"], label_title["coords"] = self.parent.align(label_title["label"], label_title["coords"],
+            #                                                                inacurr=-20, type_blit=False)
+            self.labels.append(label_title)
 
     def init_button_menu(self):
         w, h = 80, 50
